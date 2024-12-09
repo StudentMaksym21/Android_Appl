b68d4a70e299d48b49523406785dbfc89cd0acab
Maksym_Nadvornyi <ul0280452@edu.uni.lodz.pl>
2024-12-09 02:43:58 +0100

    Fragments sample(02.12)

---
+++ app/src/main/java/com/example/mobile_appl/FragmentA.kt
@@ -0,0 +1,18 @@
+package com.example.mobile_appl
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentA : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+}
--- app/src/main/java/com/example/mobile_appl/MainActivity.kt
+++ app/src/main/java/com/example/mobile_appl/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.mobile_appl
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var showingFragmentA = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            // Initially display FragmentA
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FragmentA())
+                .commit()
+        }
+
+        val buttonToggle: Button = findViewById(R.id.button_toggle)
+        buttonToggle.setOnClickListener {
+            toggleFragment()
+        }
+    }
+
+    private fun toggleFragment() {
+        val fragment: Fragment = if (showingFragmentA) {
+            FragmentB()
+        } else {
+            FragmentA()
+        }
+        showingFragmentA = !showingFragmentA
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
